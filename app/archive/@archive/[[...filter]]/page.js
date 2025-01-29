acb85a75845f25659fa331140b0ebf710a304cfe
PhillipJBridgeman <106999026+PhillipJBridgeman@users.noreply.github.com>
2025-01-29 15:32:27 -0600

    Refactor archive pages: consolidate filtering logic into a new component and remove outdated pages

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,40 @@
+import NewsList from '@/components/news-list';
+import Link from 'next/link';
+import { getNewsForYear, getAvailableNewsYears, getAvailableNewsMonths} from '@/lib/news';
+
+export default function FilteredNewsPage( {params} ) {
+    const filter = params.filter;
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    let news;
+    let links = getAvailableNewsYears();
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    let newsContent = <p>No news found for the selected period.</p>;
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
+
+    return (
+        <>
+        <header id="archive-header">
+            <nav>
+                <ul>
+                    {links.map(link => {
+                        <li key ={link}>
+                            <Link href={`/archive/${link}`}>{link}</Link>
+                        </li>
+                    })}
+                </ul>
+            </nav>
+        </header>
+        {newsContent}
+        </>
+    );
+}
