868b0a28c20784f5108371cfba09056a2fac7db0
PhillipJBridgeman <106999026+PhillipJBridgeman@users.noreply.github.com>
2025-01-30 15:03:17 -0600

    Replace Link with NavLink for News in MainHeader and add active class for Archive link

--- components/main-header.js
+++ components/main-header.js
@@ -1,6 +1,8 @@
+import NavLink from '@/components/nav-link';
 import Link from 'next/link';
 
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <header id="main-header">
       <div id="logo">
@@ -9,7 +11,10 @@ export default function MainHeader() {
       <nav>
         <ul>
           <li>
-            <Link href="/news">News</Link>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <Link href="/archive" className={path.startsWith('/archive') ? 'active' : undefined }>Archive</Link>
           </li>
         </ul>
       </nav>
