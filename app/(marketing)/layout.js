b7fce688197dc3330d24c8a439415d77ca2cf983
Phillip Bridgeman <phillipbridgeman@gmail.com>
2025-02-03 15:51:03 -0600

    Refactor layout structure and add marketing homepage; rename files for clarity

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,14 +1,16 @@
-import './globals.css';
- 
+import '../globals.css';
+
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
 };
- 
-export default function RootLayout({ children }) {
-  return (
+
+export default function MarketingLayout({ children }) {
+  return(
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
